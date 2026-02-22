e1ecc326ae9f2fdf9ff1bd29e100cd8fe4f105ff
SongWork <deploy@songwork.app>
2026-02-22 04:36:33 +0000

    fix: 담당자 선택 목록을 소속 부서 배정된 사용자만 표시 + admin도 부서 지정 가능

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2988466"
+    serviceWorkerVersion: "1445563586"
   }
 });
